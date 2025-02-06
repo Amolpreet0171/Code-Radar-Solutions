bd06982507a877d8e4228ca84ee069bdc67690e3
Amolpreet0171 <amolpreet0171.becse24@chitkara.edu.in>
2025-02-06 09:57:57 +0530

    Solution to Check Number Sign

---
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    in a;
+    scanf("%d",&a);
+    if(a>0){
+        printf("Positive");
+    }
+    else if(a<0){
+        printf("Negative");
+    }
+    else if<a==0
+    {
+    printf("Zero");
+    }    
+return 0;
+}
